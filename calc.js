0bda99cd548c03ee22a5f968e5744983719fb9c8
Denis <denis.leduc@protonmail.com>
2021-11-08 14:53:25 +0200

    Calculator js

--- calc.js
+++ calc.js
@@ -1,7 +1,58 @@
 const buttons = document.querySelectorAll('.btn');
+const display = document.querySelector('.text');
+let expressionString = '';
 
-buttons.forEach(button => button.addEventListener('click', logText));
+buttons.forEach(button => button.addEventListener('click', process));
 
-function logText(e) {
-  console.log(this.name);
+
+
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const power = (x, n) => Math.pow(x, n);
+
+
+function process(e) {
+  switch (this.name) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      expressionString += this.name;
+      break;
+
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '**':
+      expressionString += ' ' + this.name + ' ';
+      break;
+
+    case '=':
+      
+      break;
+
+    case 'reset':
+      expressionString = '';
+      break;
+
+    case 'backspace':
+      if (expressionString.charAt(expressionString.length-1) == ' ') {
+        expressionString = expressionString.slice(0, expressionString.length-3);
+      } else {
+        expressionString = expressionString.slice(0, expressionString.length-1);
+      }
+      break;
+
+  }
+  console.log('Value after processing: ' + expressionString);
+  display.textContent = expressionString;
 }
