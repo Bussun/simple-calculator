8217e7a2b481d2bc3d772dbca81947253c1b8201
Denis <denis.leduc@protonmail.com>
2021-11-08 17:48:28 +0200

    Wrote and rewrote JavaScript

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 const buttons = document.querySelectorAll('.btn');
 const display = document.querySelector('.text');
-let expressionString = '';
+let num1 = undefined, num2 = undefined, result = undefined, operator = undefined;
 
 buttons.forEach(button => button.addEventListener('click', process));
 
@@ -9,12 +9,12 @@ buttons.forEach(button => button.addEventListener('click', process));
 const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = (a, b) => (b !=0) ? a / b : display.textContent = 'Nice try';
 const power = (x, n) => Math.pow(x, n);
 
 
 function process(e) {
-  switch (this.name) {
+  /* switch (this.name) {
     case '0':
     case '1':
     case '2':
@@ -28,6 +28,8 @@ function process(e) {
       expressionString += this.name;
       break;
 
+    case '.':
+      if (expressionString.includes('.'))
     case '+':
     case '-':
     case '*':
@@ -37,7 +39,8 @@ function process(e) {
       break;
 
     case '=':
-      
+
+
       break;
 
     case 'reset':
@@ -54,5 +57,51 @@ function process(e) {
 
   }
   console.log('Value after processing: ' + expressionString);
-  display.textContent = expressionString;
+  display.textContent = expressionString;*/
+
+  switch (this.classList.contains(value)) {
+    case value == 'number':
+
+    case value == 'operators':
+      if (this.name == '=') break;
+      operator = this.name;
+      break;
+
+    case value == 'equal':
+      switch (operator) {
+        case '+':
+          reault = add(num1, num2);
+          break;
+
+        case '-':
+          result = substract(num1, num2);
+          break;
+
+        case '*':
+          result = multiply(num1, num2);
+          break;
+
+        case '**':
+          result = power(num1, num2);
+          break;
+      }
+
+    case value == 'reset':
+
+    case value == 'backspace':
+  }
+}
+
+function resetvars() {
+  num1 = undefined;
+  num2 = undefined;
+  operator = undefined;
+}
+
+function resetCalc() {
+  num1 = undefined;
+  num2 = undefined;
+  result = undefined;
+  operator = undefined;
+  display.textContent = '0';
 }
