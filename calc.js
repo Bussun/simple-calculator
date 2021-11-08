43b79c5be9a298021d1eaecb1b344a12ba0ff7d6
Denis <denis.leduc@protonmail.com>
2021-11-08 19:32:53 +0200

    Saving

--- calc.js
+++ calc.js
@@ -14,50 +14,71 @@ const power = (x, n) => Math.pow(x, n);
 
 
 function process(e) {
-  switch (this.classList.contains(value)) {
-    case value == 'number':
-      if (operator != undefined) {
+  let classListofBtn = this.classList;
+  switch (true) {
+    case classListofBtn.contains('number'):
+      if (operator == undefined) {
+        if (num1 == undefined) num1 = '';
         num1 += this.name;
-        console.log(num1);
+        display.textContent = num1;
       } else {
+        if (num2 == undefined) num2 = '';
         num2 += this.name;
+        display.textContent = num2;
       }
       break;
 
-    case value == 'operators':
-      if (this.name == '=') break;
+    case classListofBtn.contains('operators'):
+      //if (this.name == '=') break;
       if (num1 == undefined && result != 0 && result != undefined) num1 = result;
       operator = this.name;
       break;
 
-    case value == 'equal':
+    case classListofBtn.contains('equal'):
       if (num2 == undefined) break;
       switch (operator) {
         case '+':
-          reault = add(num1, num2);
+          reault = add(+num1, +num2);
           resetVars();
+          display.textContent = result;
           break;
 
         case '-':
-          result = substract(num1, num2);
+          result = substract(+num1, +num2);
           resetVars();
+          display.textContent = result;
           break;
 
         case '*':
-          result = multiply(num1, num2);
+          result = multiply(+num1, +num2);
           resetVars();
+          display.textContent = result;
           break;
 
         case '**':
-          result = power(num1, num2);
+          result = power(+num1, +num2);
           resetVars();
+          display.textContent = result;
           break;
+
+        case '/':
+          if (+num2 == 0) textContent.display = 'Can\'t divide by 0';
+          result = divide(num1, num2);
+          display.textContent = result;
       }
 
-    case value == 'reset':
+    case classListofBtn.contains('reset'):
+      resetCalc();
+      break;
 
-    case value == 'backspace':
+    case classListofBtn.contains('backspace'):
+      break;
   }
+  console.log(result);
+}
+
+function roundNumber(num) {
+  return Number.parseFloat(num).toFixed(2);
 }
 
 function resetVars() {
