4ee998debd8bc8a1fb028b6189c33ce1278aad11
Denis <denis.leduc@protonmail.com>
2021-11-08 20:14:42 +0200

    Easter egg added

--- calc.js
+++ calc.js
@@ -11,7 +11,7 @@ buttons.forEach(button => button.addEventListener('click', process));
 const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => (b !=0) ? a / b : display.textContent = 'Nice try';
+const divide = (a, b) => (b !=0) ? a / b : easterEgg();
 const power = (x, n) => Math.pow(x, n);
 
 
@@ -21,10 +21,12 @@ function process(e) {
     case classListofBtn.contains('number'):
       if (operator == undefined) {
         if (num1 == undefined) num1 = '';
+        if (this.name == '.' && num1.includes('.')) break;
         num1 += this.name;
         display.textContent = num1;
       } else {
         if (num2 == undefined) num2 = '';
+        if (this.name == '.' && num2.includes('.')) break;
         num2 += this.name;
         display.textContent = num2;
       }
@@ -38,43 +40,7 @@ function process(e) {
 
     case classListofBtn.contains('equal'):
       if (num2 == undefined) break;
-      console.log('Operator: ' + operator);
-      switch (operator) {
-        case '+':
-          result = add(+num1, +num2);
-          resetVars();
-          display.textContent = result;
-          break;
-
-        case '-':
-          result = substract(+num1, +num2);
-          resetVars();
-          display.textContent = result;
-          break;
-
-        case '*':
-          result = multiply(+num1, +num2);
-          resetVars();
-          display.textContent = result;
-          break;
-
-        case '**':
-          result = power(+num1, +num2);
-          resetVars();
-          display.textContent = result;
-          break;
-
-        case '/':
-          if (+num2 == 0) textContent.display = 'Can\'t divide by 0';
-          result = divide(num1, num2);
-          resetVars();
-          display.textContent = result;
-          break;
-
-        default:
-          display.textContent = 'Something went wrong';
-          break;
-      }
+      operate();
       break;
 
     case classListofBtn.contains('reset'):
@@ -85,6 +51,51 @@ function process(e) {
       break;
   }
   console.log('Result: ' + result);
+}
+
+function operate() {
+  console.log('Operator: ' + operator);
+  switch (operator) {
+    case '+':
+      result = add(+num1, +num2);
+      resetVars();
+      display.textContent = result;
+      break;
+
+    case '-':
+      result = substract(+num1, +num2);
+      resetVars();
+      display.textContent = result;
+      break;
+
+    case '*':
+      result = multiply(+num1, +num2);
+      resetVars();
+      display.textContent = result;
+      break;
+
+    case '**':
+      result = power(+num1, +num2);
+      resetVars();
+      display.textContent = result;
+      break;
+
+    case '/':
+      if (+num2 == 0) {
+        display.textContent = 'Nice try';
+        result = undefined;
+        resetVars();
+        break;
+      }
+      result = divide(num1, num2);
+      resetVars();
+      display.textContent = result;
+      break;
+
+    default:
+      display.textContent = 'Something went wrong';
+      break;
+  }
 }
 
 function roundNumber(num) {
@@ -103,4 +114,10 @@ function resetCalc() {
   result = undefined;
   operator = undefined;
   display.textContent = '0';
+}
+
+function easterEgg() {
+  buttons.forEach(button => button.textContent = '0');
+  display.style.fontSize = '15px';
+  display.textContent = 'You like 0, don\'t you ? Now you need to reaload the page to continue';
 }
