5acebac1036910b3b62abb5f151d9652eeab94ea
Denis <93516910+Bussun@users.noreply.github.com>
2021-12-21 22:38:00 +0100

    Added keyboard support
    Isn't really optimized tho
    I think there is a better way to achieve this

--- calc.js
+++ calc.js
@@ -1,20 +1,108 @@
 const buttons = document.querySelectorAll('.btn');
 const display = document.querySelector('.text');
 const operatorDisplay = document.querySelector('.operator-display');
-let num1 = undefined, num2 = undefined, result = undefined, operator = undefined, illegalOperation = false;
+let num1 = undefined,
+  num2 = undefined,
+  result = undefined,
+  operator = undefined,
+  illegalOperation = false;
 
 resetCalc();
 
-buttons.forEach(button => button.addEventListener('click', process));
-
-
+buttons.forEach((button) => button.addEventListener('click', process));
+window.addEventListener('keydown', (e) => processKey(e.key));
 
 const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => (b !=0) ? a / b : easterEgg();
+const divide = (a, b) => (b != 0 ? a / b : easterEgg());
 const power = (x, n) => Math.pow(x, n);
 
+function processKey(key) {
+  switch (key) {
+    case '1':
+    case '2': 
+    case '3': 
+    case '4': 
+    case '5': 
+    case '6': 
+    case '7': 
+    case '8': 
+    case '9':
+    case '0':
+      console.log(key);
+      if (operator == undefined) {
+        if (num1 == undefined) num1 = '';
+        if (this.name == '.' && num1.includes('.')) break;
+        if (num1.length >= 9) break;
+        num1 += key;
+        display.textContent = num1;
+      } else {
+        if (num2 == undefined) num2 = '';
+        if (this.name == '.' && num2.includes('.')) break;
+        if (num2.length >= 9) break;
+        num2 += key;
+        display.textContent = num2;
+      }
+      break;
+
+    case '+': 
+    case '-': 
+    case '*': 
+    case '/':
+    case '^':
+      console.log(key);
+      if (num1 == undefined && result != 0 && result != undefined)
+        num1 = result;
+      if (
+        num1 == undefined &&
+        num2 == undefined &&
+        result == undefined &&
+        key == '-'
+      ) {
+        num1 = 0;
+        operator = key;
+        setOperatorDisplay();
+        break;
+      }
+      if (num1 == undefined && result == undefined) break;
+      if (num2 != undefined) {
+        operate();
+        if (illegalOperation) {
+          illegalOperation = false;
+          break;
+        }
+        operator = key;
+        setOperatorDisplay();
+        num1 = result;
+        break;
+      }
+      if (key == '^') operator = '**'
+      operator = key;
+      setOperatorDisplay();
+      break;
+
+    case '=':
+    case 'Enter':
+      if (num2 == undefined) break;
+      operate();
+      break;
+
+    case 'r':
+      resetCalc();
+      break;
+
+    case 'Backspace':
+      if (num2 == undefined) {
+        num1 = num1.slice(0, num1.length - 1);
+        display.textContent = num1;
+        break;
+      }
+      num2 = num2.slice(0, num2.length - 1);
+      display.textContent = num2;
+      break;
+  }
+}
 
 function process() {
   let classListofBtn = this.classList;
@@ -36,8 +124,14 @@ function process() {
       break;
 
     case classListofBtn.contains('operators'):
-      if (num1 == undefined && result != 0 && result != undefined) num1 = result;
-      if (num1 == undefined && num2 == undefined && result == undefined && classListofBtn.contains('substract')) {
+      if (num1 == undefined && result != 0 && result != undefined)
+        num1 = result;
+      if (
+        num1 == undefined &&
+        num2 == undefined &&
+        result == undefined &&
+        classListofBtn.contains('substract')
+      ) {
         num1 = 0;
         operator = this.name;
         setOperatorDisplay();
@@ -70,11 +164,11 @@ function process() {
 
     case classListofBtn.contains('backspace'):
       if (num2 == undefined) {
-        num1 = num1.slice(0, num1.length-1);
+        num1 = num1.slice(0, num1.length - 1);
         display.textContent = num1;
         break;
       }
-      num2 = num2.slice(0, num2.length-1);
+      num2 = num2.slice(0, num2.length - 1);
       display.textContent = num2;
       break;
   }
@@ -169,7 +263,7 @@ function resetCalc() {
 }
 
 function easterEgg() {
-  buttons.forEach(button => button.textContent = '0');
+  buttons.forEach((button) => (button.textContent = '0'));
   display.style.fontSize = '15px';
   display.textContent = 'You like 0, don\'t you ? (Reload to continue.)';
 }
